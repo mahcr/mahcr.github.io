6f8f76c700c1ccf9a643a27b8c372094117aee81
mariano alvarez <malvarezhcr@gmail.com>
2017-03-31 22:36:51 -0600

    update data of project

--- config/data/index.data.js
+++ config/data/index.data.js
@@ -4,12 +4,19 @@ export default {
   prefix: 'cv',
   head: {
     title: 'Mariano Alvarez | FE Dev',
+    ogType: 'website',
+    ogUrl: 'http://mahcr.github.io/',
+    ogImg: '',
   },
-  img: 'http://placehold.it/350x350',
+  img: 'src/app/assets/imgs/me.jpg',
   imgAlt: 'Mariano Alvarez Profile image',
   figureCaption: 'Yep, That\'s me :D',
   name: 'Mariano Alvarez',
   jobTitle: 'Front-End Dev',
   passion: 'Love: ',
-  description: 'I\'m a Front-End developer who love Javascript and new technologies. I consider myself as a easy going person and with a spark of fun. Some of my hobbies are: Play Xbox, go to off-road rides and discover new beers.'
+  description: 'I\'m a Front-End developer who love Javascript and new technologies. I consider myself as a easy going person and with a spark of fun. Some of my hobbies are: Play Xbox, go to off-road rides and discover new beers.',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/mahcr',
+    mail: 'mailto:malvarezhcr@gmail.com'
+  }
 }
