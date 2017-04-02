3d5920dd60bf73cccbfd687343f8736200fb33f8
mariano alvarez <malvarezhcr@gmail.com>
2017-04-01 20:06:40 -0600

    update files

---
+++ src/dev.js
@@ -0,0 +1,4 @@
+/**
+ * Dev file to include ejs template so webpack if aware of the changes done in those files
+ */
+import './index.ejs'
--- src/vendors.js
+++
@@ -1,7 +0,0 @@
-import './theme/global.scss';
-import './index.ejs';
-
-// third party libraries
-// console.log('hola consola');
-
-
